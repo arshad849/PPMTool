91aa9f97e18dd4f2ed0980909cd8e7533d545e2c
arshad <arshad849@gmail.com>
2020-06-13 16:42:58 +0530

    First Commit

---
+++ rest-services/src/main/java/com/todos/restservices/todos/TodoJPARepository.java
@@ -0,0 +1,15 @@
+package com.todos.restservices.todos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoJPARepository extends JpaRepository<Todo, Long> {
+
+	List<Todo> findByUsername(String username);
+	
+	
+
+}
---
+++ rest-services/src/test/java/com/todos/restservices/RestServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todos.restservices;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
